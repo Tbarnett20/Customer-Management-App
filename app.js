6b7bb68eff274116ae4d5bc2730ba84c76a37244
Terrance <tbarnett2020@gmail.com>
2018-08-23 06:50:49 -0400

    Added Forms & Input **About to add express-validator**

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ let bodyParser = require('body-parser');
 let path = require('path')
 
 let app = express();
-/*
-let logger = function(req, res, next) {
+/* let logger = function(req, res, next) {
   console.log('Logging...');
   next();
 }
+app.use(logger); */
 
-app.use(logger);
-
-*/
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -20,9 +20,39 @@ app.use(bodyParser.urlencoded({exyended: false}))
 // Set Static Path
 app.use(express.static(path.join(__dirname, 'public')))
 
+let users = [
+  { id: 1,
+    first_name: 'Terry',
+    last_name: 'Barnett',
+    email: 'terrance@gmail.com'
+  },
+  { id: 2,
+    first_name: 'Crabby',
+    last_name: 'Watts',
+    email: 'crabby@gmail.com'
+  },
+  { id: 3,
+    first_name: 'Katie',
+    last_name: 'Whats',
+    email: 'katie@gmail.com'
+  },
+]
+
 // Routes
 app.get('/', function (req, res) {
-  res.send('Hello');
+  res.render('index', {
+    title: 'Customers',
+    users: users
+  });
+});
+
+app.post('/users/add', function (req, res){
+  let newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+  }
+  console.log(newUser)
 });
 
 app.listen(3000, function () {
