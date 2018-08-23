f89943e65b1cf77b6522c807c01161f4dc2ecaa1
Terrance <tbarnett2020@gmail.com>
2018-08-23 05:36:43 -0400

    Performed Basic Routing**

--- app.js
+++ app.js
@@ -3,6 +3,27 @@ let bodyParser = require('body-parser');
 let path = require('path')
 
 let app = express();
+/*
+let logger = function(req, res, next) {
+  console.log('Logging...');
+  next();
+}
+
+app.use(logger);
+
+*/
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({exyended: false}))
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.get('/', function (req, res) {
+  res.send('Hello');
+});
 
 app.listen(3000, function () {
   console.log('Server Started on Port 3000..')
