ad164afac50702ccde53df6270352096581d0383
Terrance <tbarnett2020@gmail.com>
2018-08-23 05:03:52 -0400

    Basic Express App

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path')
+
+let app = express();
+
+app.listen(3000, function () {
+  console.log('Server Started on Port 3000..')
+})
